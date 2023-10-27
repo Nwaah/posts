73c36ea5d837acd528fb20abfc493cd27704978f
Nwaah <dkrol1@poczta.onet.pl>
2023-10-27 18:18:29 +0200

    Post routes + login/register routes for auth later

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// posts
+Route::controller(PostController::class)->group(function () {
+    Route::any('/post', 'index');
+    Route::get('/post/new', 'create');
+    Route::post('/post/save', function(Request $request) {
+        return (new PostController())->store($request);
+    });
+    Route::get('/post/{post}', function(Post $post){
+        return (new PostController())->show($post->id);
+    });
+    Route::get('/post/edit/{post}', function(Post $post){
+        return (new PostController())->edit($post);
+    });
+});
+
+// auth
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::post('/register', function($request) {
+    // save new user
+});
+
+Route::any('/login', function($request) {
+    return Auth::authenticate($request->login, $request->password);
 });
