a201e89e30708ad3535d33820578003d161336af
Nwaah <dkrol1@poczta.onet.pl>
2023-10-28 15:22:45 +0200

    Moved to Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -17,6 +18,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 // posts
 Route::controller(PostController::class)->group(function () {
     Route::any('/post', 'index');
@@ -32,15 +41,10 @@ Route::controller(PostController::class)->group(function () {
     });
 });
 
-// auth
-Route::get('/register', function () {
-    return view('register');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::post('/register', function($request) {
-    // save new user
-});
-
-Route::any('/login', function($request) {
-    return Auth::authenticate($request->login, $request->password);
-});
+require __DIR__.'/auth.php';
