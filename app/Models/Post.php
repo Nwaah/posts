fa30a3bbaac2d7380b06879b1ede408a203faed0
Nwaah <dkrol1@poczta.onet.pl>
2023-11-10 18:57:15 +0100

    Added admin flag to users Added reference to User for Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -17,4 +18,8 @@ class Post extends Model
         'user_id',
         'content'
     ];
+
+    public function user() :BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
